e7c2bf7d902dbc67eee2f423ce01ae5c4bdbfaf8
kazuki.yanagisawa <syoka467@gmail.com>
2020-09-18 01:04:03 +0000

    first commit

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  
   validates :content, presence: true, length: { maximum: 255 }
+  
+  has_many :favorites, dependent: :destroy,foreign_key: 'micropost_id'
+  has_many :likes, through: :favorites
 end
